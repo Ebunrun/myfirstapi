7319d04db95ec98b04aa610a9c4f4a2eaedb256b
Ebunrun <128905614+Ebunrun@users.noreply.github.com>
2023-04-02 19:51:45 +0100

    updated json data

--- app.js
+++ app.js
@@ -22,8 +22,20 @@ app.get('/get-time', (request, response) => {
 })
 
 app.get('/winner', (req, res) => {
-  const winner = { name: 'John Doe', phoneNumber: 08030512688, age: 30 };
-  console.log(time)
+  const winner = [
+    { id: 0, name: 'John Olaniyi', email: 'john@olaniyi.com', phone: '08030512688', age: 28 },
+    { id: 1, name: 'Ebun Goodness', email: 'ebun@goodness.com', phone: '08030512688', age: 25 },
+    { id: 2, name: 'Ayuba Ezekiel', email: 'ayuba@ezekiel.com', phone: '08030512688', age: 30 },
+    { id: 3, name: 'Shalom Olamide', email: 'shalom@olamide.com', phone: '08030512688', age: 18 },
+    { id: 4, name: 'Chioma Onag', email: 'chioma@onag.com', phone: '08030512688', age: 20 },
+    { id: 5, name: 'Terv Igbo', email: 'terve@igbo.com', phone: '08030512688', age: 30 },
+    { id: 6, name: 'Esther Babs', email: 'esther@babs.com', phone: '08030512688', age: 35 },
+    { id: 7, name: 'Precious Ohio', email: 'precious@ohio.com', phone: '08030512688', age: 19 },
+    { id: 8, name: 'Dupe Ehiorobho', email: 'dupe@ehiorobho.com', phone: '08030512688', age: 22 },
+    { id: 9, name: 'Destiny Oraifor', email: 'destiny@oraifor.com', phone: '08030512688', age: 31 },
+    { id: 10, name: 'Martins Adeoti', email: 'martins@adeoti.com', phone: '08030512688', age: 37 },
+  ];
+  console.log(winner)
   res.json(winner);
 })
 
